5e522eac39c67c4a09213f40b9159f3e7a4e9860
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-04 15:11:42 +0200

    Finish the Route implementation

--- src/ija/map/map_src/Route.java
+++ src/ija/map/map_src/Route.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Source code for the final IJA project
  * Line class
  * (C) Lukas Javorsky (xjavor20)
@@ -8,32 +8,73 @@
 
 package ija.map.map_src;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.AbstractMap.SimpleImmutableEntry;
 
-
+/**
+ * Represents the Route that will Line follow
+ * The route contains ordered streets and stops that defines the route
+ */
 public class Route{
-
-    
+    /// Route that the Line will take
     private List<SimpleImmutableEntry<Coordinate, Street>> route;
+    /// List of the stops within the route
     private List<Stop> stops;
+    /// List of the streets within the route in order
+    private List<Street> streets;
 
     /**
-     * Constructor
+     * Constructor for the Route
      */
-    private Route(List<Street> streets, List<Stop> stops_to_visit){
+    private Route(List<Street> streets_to_visit, List<Stop> stops_to_visit, Coordinate starting_point){
+        this.stops = stops_to_visit;
+        this.streets = streets_to_visit;
+
+        // Fill the route
         for (Street s : streets) {
-            List<Coordinate> c = s.getCoordinates();
-            for (Coordinate current_c : c) {
-                SimpleImmutableEntry<Coordinate, Street> tmp_position = new SimpleImmutableEntry<Coordinate, Street>(c, s);
-                
-                // Add possition to the route
-                this.route.add(tmp_position);
+            List<Coordinate> coordinates_in_street = s.getCoordinates();
+            boolean backwards_coordinates = false;
+
+            // We are going from end of the street to the beginning, so coordinates have to be reordered
+            if (starting_point == s.end_of_the_street() || coordinates_in_street.get(0) == s.end_of_the_street()){
+                // Set flag
+                backwards_coordinates = true;
+            }
+
+            for (Coordinate current_c : coordinates_in_street) {
+                SimpleImmutableEntry<Coordinate, Street> tmp_position = new SimpleImmutableEntry<Coordinate, Street>(current_c, s);
+
+                if (backwards_coordinates){
+                    List<SimpleImmutableEntry<Coordinate, Street>> tmp_route = new ArrayList<SimpleImmutableEntry<Coordinate, Street>>();
+                    tmp_route.add(tmp_position);
+
+                    // Backwards iteration
+                    for (int i = tmp_route.size(); i-- > 0;){
+                        this.route.add(tmp_route.get(i));
+                    }
+                }
+                else {
+                    // Just add possition to the route
+                    this.route.add(tmp_position);
+                }  
             }
         }
     }
 
-    public Route defaultRoute(List<Street> streets, List<Stop> stops_to_visit){
+    /**
+     * Creates the Route, if the parameters are correct
+     * @param streets Which streets does the route cointain, must be ordered
+     * @param stops_to_visit Which stops does the route contain, must be ordered
+     * @param starting_point Wehre does the route begins
+     * @return Route object, or null in case that the streets are not connected, or starting_point is not the beginning
+     * or end of the first street in the streets
+     */
+    public Route defaultRoute(List<Street> streets, List<Stop> stops_to_visit, Coordinate starting_point){
+        if (!(starting_point == streets.get(0).begin_of_the_street() || starting_point == streets.get(0).end_of_the_street())){
+            return null;
+        }
+
         Street previous_street = null;
         for (Street s : streets) {
 
@@ -45,19 +86,63 @@ public class Route{
             }
             previous_street = s;
         }
-        return new Route(streets, stops_to_visit);
+        return new Route(streets, stops_to_visit, starting_point);
     }
 
     /**
-     * 
-     * @return
+     * Getter for the starting possition possition
+     * @return Where the route starts
      */
     public SimpleImmutableEntry<Coordinate, Street> get_first(){
-
+        return this.route.get(0);
     }
 
-    public SimpleImmutableEntry<Coordinate, Street> get_next(){
+    /**
+     * Getter for the next possition of the route
+     * @param current_possition Current possition, where the Line is
+     * @return Next possition for Line, or null if the current_possition is not part of the route
+     */
+    public SimpleImmutableEntry<Coordinate, Street> get_next(SimpleImmutableEntry<Coordinate, Street> current_possition){
+        int index = this.route.indexOf(current_possition);
 
+        if (index != -1){
+            return this.route.get(index + 1);
+        }
+        else{
+            // The current possition is not part of the route
+            return null;
+        }
     }
 
+    /**
+     * Getter for the stops in route
+     * They should be ordered
+     * @return List of stops
+     */
+    public List<Stop> get_stops(){
+        return this.stops;
+    }
+
+    /**
+     * Getter for the streets in route
+     * They are ordered
+     * @return List of streets
+     */
+    public List<Street> get_streets(){
+        return this.streets;
+    }
+
+    /**
+     * Returns if the Line should stop on given coordinate
+     * @param c Coordinate that may be the stop in route
+     * @return true or false, acording to the result
+     */
+    public boolean shouldStop (Coordinate c){
+        for (Stop stop : this.stops) {
+            if (c == stop.getCoordinate()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
