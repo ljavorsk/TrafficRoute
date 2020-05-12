4e4aead6cff0a554a0ab93ba916c516701bf3e28
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-12 17:30:16 +0200

    Start work on GUI

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,2 +1,33 @@
+/*
+ * Source code for the final IJA project
+ * MainController class
+ * (C) Lukas Javorsky (xjavor20)
+ * (C) Patrik Ondriga (xondri08)
+ *
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.Pane;
+
+/**
+ * Controls the interaction with the user
+ */
 public class MainController {
+    /// Main content pane
+    @FXML
+    private Pane main_content;
+
+    /**
+     * Handler for the scroll zooming
+     * @param event Events in Scroll Pane
+     */
+    @FXML
+    private void zoom(ScrollEvent event){
+        event.consume();
+        double zoom_by = 0.15;
+        double zoom_value = event.getDeltaY() > 0 ? (1 + zoom_by) : (1 - zoom_by);
+        main_content.setScaleX(zoom_value * main_content.getScaleX());
+        main_content.setScaleY(zoom_value * main_content.getScaleY());
+    }
 }
