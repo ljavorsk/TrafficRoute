7f0fb682740f6ab2836307eedb817bfbdf9a841b
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-13 20:57:50 +0200

    Add stops, title to the main and adapt bus to changes in constructor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,13 +14,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import map.Map;
-import map.map_src.Bus;
-import map.map_src.Coordinate;
-import map.map_src.Drawable;
-import map.map_src.Street;
+import map.map_src.*;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -36,28 +33,35 @@ public class Main extends Application {
         BorderPane window = loader.load();
         Scene scene = new Scene(window);
         primaryStage.setScene(scene);
+
         primaryStage.show();
 
         Map map = new Map("Brno");
         boolean result = map.loadData("./data/street.json", "./data/line.json");
-        System.out.println(result);
+        primaryStage.setTitle("Mapa mesta: " + map.getTownName());
         List<Street> streets = map.getStreets();
+        List<Stop> stops = map.getStops();
 
-        MainController controller = loader.getController();
         Coordinate c1,c2;
-        c1 = Coordinate.create(100, 100);
-        c2 = Coordinate.create(100, 150);
+        Street s1 = streets.get(0);
+        c1 = s1.beginOfTheStreet();
+        c2 = s1.endOfTheStreet();
+        AbstractMap.SimpleImmutableEntry<Coordinate, Street> start_p1 =
+                new AbstractMap.SimpleImmutableEntry<Coordinate, Street>(c1, s1);
+        AbstractMap.SimpleImmutableEntry<Coordinate, Street> start_p2 =
+                new AbstractMap.SimpleImmutableEntry<Coordinate, Street>(c2, s1);
 
 
-        Bus b1 = new Bus("SD102", c1, 2);
-        Bus b2 = new Bus("SD102", c2, 2);
-
+        Bus b1 = new Bus("SD102", start_p1);
+        Bus b2 = new Bus("SD102", start_p2);
 
         List<Drawable> elements = new ArrayList<>();
+        elements.addAll(streets);
+        elements.addAll(stops);
         elements.add(b1);
         elements.add(b2);
-        elements.addAll(streets);
 
+        MainController controller = loader.getController();
         controller.setDrawings(elements);
     }
 }
