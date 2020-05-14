819335981a860495bee84863496d070a420601e8
Patrik Ondriga <xondri08@vutbr.cz>
2020-05-14 21:23:16 +0200

    Create custom buttons for streets and lines

---
+++ src/main/java/gui/LineButton.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javafx.scene.control.Button;
+import map.map_src.Line;
+
+public class LineButton extends Button {
+    private final Line line;
+
+    public LineButton(Line line){
+        this.line = line;
+        this.setText(String.valueOf(line.getId()));
+        this.setMinSize(50, 20);
+    }
+
+    public Line getLine() {
+        return line;
+    }
+}
